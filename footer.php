e925dae6d3c5c127f2c70fbe4b9d27c6498195d3
Ian Rastall <moving.form.of.dust@gmail.com>
2024-07-16 22:02:21 -0500

    Fixed everything

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-                    <div class="icon-link">
-                            <p><img src="images/icon-copyleft.png" alt="copyleft symbol"> 2024 No rights reserved</p>
-                    </div>
-</footer>
